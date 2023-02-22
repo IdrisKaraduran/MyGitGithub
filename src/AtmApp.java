bef5e63f62c522da59e7c31567f555a5349b7eed
idris <ninoreyesmanuel@gmail.com>
2023-02-22 20:28:19 +0100

    Branch Versiyon

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -7,6 +7,13 @@ public class AtmApp {
 
 
         System.out.println("Git Cok kolay");
+        System.out.println("Wer hat was gesagt");
+        System.out.println("Ich habe ihr am Montag in Rostock gesagt");
+        System.out.println("Willst du mich heiratet?");
+        System.out.println("Sie hat mir Ja gesagt");
+        System.out.println("Danach haben wir geheiratet");
+        System.out.println("Am Ende hatten wir traurig");
+
     }
 
 }
