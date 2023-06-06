4fdef9fe87b6238d805e781c63abdac5b0edeaea
IdrisKaraduran <122121825+IdrisKaraduran@users.noreply.github.com>
2023-06-06 22:25:32 +0200

    Update AtmApp.java

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -5,21 +5,6 @@ public class AtmApp {
 
         System.out.println("Lerne Java und verdine viel Geld");
 
-
-        System.out.println("Git Cok kolay");
-        System.out.println("Wer hat was gesagt");
-        System.out.println("Ich habe sie am Montag in Rostock gefragt");
-        System.out.println("Willst du mich heiraten?");
-        System.out.println("Sie hat mir Ja gesagt");
-        System.out.println("Danach haben wir geheiratet");
-        System.out.println("Am Ende hatten wir traurig");
-
-        System.out.println("Master branch de ozellik var");
-
-        System.out.println( "idris hat  meine Frage nicht gewusst");
-
-        System.out.println("Esra erinnert dich meine Liebe");
-        System.out.println("Idris");
     }
 
 }
